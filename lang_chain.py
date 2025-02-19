e3a29591da1eb7a255645d0fe922c8561fbef638
henriqueSpencer <henriquespencer11@gmail.com>
2025-02-19 18:44:08 -0300

    feat: add integration between Telegram and LangChain

--- lang_chain.py
+++ lang_chain.py
@@ -16,31 +16,31 @@ class GeminiPipeline:
         )
         # Criar três sub-prompts separados
         prompt_identificar = PromptTemplate.from_template(
-            "Essa mensagem contém um gasto (Responda com `true` ou `false`)? {mensagem}")
-        prompt_valor = PromptTemplate.from_template(
-            "Se houver um gasto na mensagem '{mensagem}', qual é o valor? senao retorn `null`")
-        prompt_categoria = PromptTemplate.from_template("""
+            "Essa mensagem contém um gasto (Responda com `True` ou `False`)? {mensagem}")
+        prompt_amount = PromptTemplate.from_template(
+            "Se houver um gasto na mensagem '{mensagem}', qual é o amount(apenas o numero)? senao retorn `null`")
+        prompt_category = PromptTemplate.from_template("""
         Dada a seguinte mensagem: "{mensagem}"
-        Qual categoria melhor se encaixa? Escolha entre:
+        Qual category melhor se encaixa? Escolha entre:
         Housing, Transportation, Food, Utilities, Insurance, Medical/Healthcare, Savings, Debt, Education, Entertainment, Other.
         caso nao se encaixe em nenhuma retorn null
         """)
 
         # Criar Chains separadas
         self.chain_identificar = LLMChain(llm=self.llm, prompt=prompt_identificar)
-        self.chain_valor = LLMChain(llm=self.llm, prompt=prompt_valor)
-        self.chain_categoria = LLMChain(llm=self.llm, prompt=prompt_categoria)
+        self.chain_amount = LLMChain(llm=self.llm, prompt=prompt_amount)
+        self.chain_category = LLMChain(llm=self.llm, prompt=prompt_category)
 
     def run_all(self, message):
         mensagem_teste = "Gastei 100 reais no Uber hoje."
 
-        identificador = self.chain_identificar.run(mensagem_teste)
-        valor = self.chain_valor.run(mensagem_teste)
-        categoria = self.chain_categoria.run(mensagem_teste)
+        is_expense = self.chain_identificar.run(mensagem_teste)
+        amount = self.chain_amount.run(mensagem_teste)
+        category = self.chain_category.run(mensagem_teste)
         return {
-            "summary": identificador,
-            "analysis": valor,
-            "insights": categoria,
+            "is_expense": bool(is_expense) if is_expense != "null" else None,
+            "amount": float(amount) if amount != "null" else None,
+            "category": category,
         }
 
 class Message:
@@ -51,37 +51,40 @@ class Message:
         self.texto = texto
         self.user_id = user_id
         self.is_expense = False
-        self.valor = None
-        self.categoria = None
+        self.amount = None
+        self.category = None
 
     def __str__(self):
         """Retorna uma representação em string do objeto."""
-        return f"Mensagem(user_id={self.user_id}, texto='{self.texto}', is_expensas={self.is_expensas}, valor={self.valor}, categoria='{self.categoria}')"
+        return f"Mensagem(user_id={self.user_id}, texto='{self.texto}', is_expensas={self.is_expensas}, amount={self.amount}, category='{self.category}')"
 
-    def definir_despesa(self, is_expense: bool, valor: float, categoria: str):
+    def definir_despesa(self, is_expense: bool, amount: float, category: str):
         """
         Define a mensagem como uma despesa e adiciona os detalhes.
 
-        :param valor: Valor da despesa.
-        :param categoria: Categoria da despesa.
+        :param amount: amount da despesa.
+        :param category: Categoria da despesa.
         """
-        self.check_mensage(is_expense, valor, categoria)
+        self.check_mensage(is_expense, amount, category)
         self.is_expense = True
-        self.valor = valor
-        self.categoria = categoria
+        self.amount = amount
+        self.category = category
 
-    def check_mensage(self, is_expense, valor, categoria):
+    def check_mensage(self, is_expense: bool, amount:float, category:str):
         if not is_expense:
             raise ValueError("this is not a bill.")
-        elif valor is None and categoria is None:
+        elif amount is None and category is None:
             msg_returned = "is been impossible to retrieve: "
-            if valor is None:
+            if amount is None:
                 msg_returned += "the value from your expenditures,"
-            if valor is None:
+            if amount is None:
                 msg_returned += "the category from your expenditures, "
             raise ValueError(f"{msg_returned}\n please improve the text and send again.")
 
-class db_maneger:
+    def check_user(self):
+        return DBmanager().select_user(self.user_id)
+
+class DBmanager:
     def __init__(self):
         pass
 
@@ -98,25 +101,38 @@ class db_maneger:
     def delete(self, user_id):
         pass
 
+    def select_user(self, user_id):
+        users = [1234, 3333,2900]
+        if user_id in users:
+            return True
+        return False
+
 if __name__ == '__main__':
 
-
+    from picklefy import PickleFy
     try:
         msg = Message("ola", 1234)
-        llm = GeminiPipeline()
+        if not msg.check_user():
+            raise ValueError("User not found")
 
-        info = llm.run_all(msg.texto)
+        # llm = GeminiPipeline()
+        # info = llm.run_all(msg.texto)
+        # PickleFy().serialize(file_name='info', variavel=info)
+
+        info = PickleFy().serialize(file_name='info')
         try:
-            msg.definir_despesa(info['identificador'], info['valor'], info['categoria'])
+            msg.definir_despesa(info['is_expense'], info['amount'], info['category'])
         except ValueError as e:
             raise e
         try:
-            db = db_maneger()
+            db = DBmanager()
             db.save(msg)
         except Exception as e:
             e.message = f"Error to save the message: {e.message}"
             raise e
 
+        # send telegram mensage with the success
+        # "[Category] expense added ✅"
     except Exception as e:
         # send telegram mensage with the error
         pass
--- main.py
+++ main.py
@@ -1,35 +1,93 @@
-#from google.auth import message
-from google.auth import message
+
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
-
-import google.generativeai as genai
 import os
-import api_keys
-import json
-import re
+import config
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from picklefy import PickleFy
 
 
-async def start(update: Update, context):
-    await update.message.reply_text("Olá! Eu sou um bot simples.")
 
 
-async def echo(update: Update, context):
-    texto_recebido = update.message.text
-    await update.message.reply_text(f"Você disse: {texto_recebido}")
+class TelegramBot:
+    def __init__(self):
+        self.app = Application.builder().token(os.getenv("TELEGRAM_API_TOKEN")).build()
 
+    def main(self):
+        self.app = Application.builder().token(os.getenv("TELEGRAM_API_TOKEN")).build()
 
-def main():
-    app = Application.builder().token(os.getenv("TELEGRAM_API_TOKEN")).build()
+        # Adicionar comandos e respostas
+        self.app.add_handler(CommandHandler("start", self.start))
+        self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.echo))
 
-    # Adicionar comandos e respostas
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+        print("Bot está rodando...")
+        self.app.run_polling()
 
-    print("Bot está rodando...")
-    app.run_polling()
+    async def start(self, update: Update, context):
+        """
+        Função para responder ao comando /start.
+        :param update:
+        :param context:
+        :return:
+        """
+        await update.message.reply_text("Olá! Eu sou um bot simples.")
 
+    async def echo(self, update: Update, context):
+        """
+        Função para responder com o mesmo texto recebido.
+        :param update:
+        :param context:
+        :return:
+        """
+        try:
+            menssage_text = update.message.text
+            message_id = update.message.message_id
+            user_name = update.message.chat.full_name
+            chat_id = update.message.chat.id
+            msg = handler_mensage(menssage_text, chat_id)
+            await update.message.reply_text(f"{msg.category} expense added ✅")
+        except ValueError as e:
+            # send telegram mensage with the error
+            await update.message.reply_text(str(e))
 
+class GeminiPipeline:
+    def __init__(self):
+        self.llm = ChatGoogleGenerativeAI(
+            model="gemini-1.5-flash",
+            temperature=0,
+            max_tokens=None,
+            timeout=None,
+            max_retries=2,
+        )
+        # Criar três sub-prompts separados
+        prompt_identificar = PromptTemplate.from_template(
+            "Essa mensagem contém um gasto (Responda com `True` ou `False`)? {mensagem}")
+        prompt_amount = PromptTemplate.from_template(
+            "Se houver um gasto na mensagem '{mensagem}', qual é o amount(apenas o numero)? senao retorn `null`")
+        prompt_category = PromptTemplate.from_template("""
+        Dada a seguinte mensagem: "{mensagem}"
+        Qual category melhor se encaixa? Escolha entre:
+        Housing, Transportation, Food, Utilities, Insurance, Medical/Healthcare, Savings, Debt, Education, Entertainment, Other.
+        caso nao se encaixe em nenhuma retorn null
+        """)
+
+        # Criar Chains separadas
+        self.chain_identificar = LLMChain(llm=self.llm, prompt=prompt_identificar)
+        self.chain_amount = LLMChain(llm=self.llm, prompt=prompt_amount)
+        self.chain_category = LLMChain(llm=self.llm, prompt=prompt_category)
+
+    def run_all(self, message):
+
+        is_expense = self.chain_identificar.run(message)
+        amount = self.chain_amount.run(message)
+        category = self.chain_category.run(message)
+        return {
+            "is_expense": True if is_expense == "True" else False,
+            "amount": float(amount) if amount != "null" else None,
+            "category": str(category) if category != "null" else None,
+        }
 
 class Message:
     def __init__(self, texto: str, user_id: int):
@@ -39,94 +97,94 @@ class Message:
         self.texto = texto
         self.user_id = user_id
         self.is_expense = False
-        self.valor = None
-        self.categoria = None
+        self.amount = None
+        self.category = None
 
     def __str__(self):
         """Retorna uma representação em string do objeto."""
-        return f"Mensagem(user_id={self.user_id}, texto='{self.texto}', is_expensas={self.is_expensas}, valor={self.valor}, categoria='{self.categoria}')"
+        return f"Mensagem(user_id={self.user_id}, texto='{self.texto}', is_expensas={self.is_expensas}, amount={self.amount}, category='{self.category}')"
 
-    def definir_despesa(self, valor: float, categoria: str):
+    def definir_despesa(self, is_expense: bool, amount: float, category: str):
         """
         Define a mensagem como uma despesa e adiciona os detalhes.
 
-        :param valor: Valor da despesa.
-        :param categoria: Categoria da despesa.
+        :param amount: amount da despesa.
+        :param category: Categoria da despesa.
         """
+        self.check_mensage(is_expense, amount, category)
         self.is_expense = True
-        self.valor = valor
-        self.categoria = categoria
+        self.amount = amount
+        self.category = category
 
-class GeminiIA:
-    def __init__(self):
-        # Configurar o cliente da API
-        genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+    def check_mensage(self, is_expense: bool, amount:float, category:str):
+        if not is_expense:
+            raise ValueError("this is not a bill.")
+        elif amount is None or category is None:
+            # TODO: improve this mensage
+            """It has been impossible to retrieve the value 
+            and category from your expenditures. 
+            Please refine the text and send it again."""
+            msg_returned = "is been impossible to retrieve: "
+            if amount is None:
+                msg_returned += "the value from your expenditures,"
+            if amount is None:
+                msg_returned += "the category from your expenditures, "
+            raise ValueError(f"{msg_returned}\n please improve the text and send again.")
 
-        # Carregar o modelo Gemini
-        self.model = genai.GenerativeModel("gemini-1.5-flash")
+    def check_user(self):
+        return DBmanager().select_user(self.user_id)
 
-    def get_expense(self, message):
-        prompt = f"""
-        Analise a mensagem abaixo e responda três perguntas separadamente, sempre retornando um JSON **válido**.
-
-        Mensagem: "{message.texto}"
-
-        Responda as seguintes perguntas:
-        1. Essa mensagem contém informações de gasto? (Responda com `true` ou `false`).
-        2. Se sim, retorne o valor do gasto identificado. Se não houver gasto, retorne `null`.
-        3. Se sim, retorne a categoria correspondente entre as seguintes opções:
-           ["Housing", "Transportation", "Food", "Utilities", "Insurance", "Medical/Healthcare", "Savings",
-           "Debt", "Education", "Entertainment", "Other"]. Se não houver gasto, retorne `null`.
-
-        ### **IMPORTANTE:**  
-        - A resposta **DEVE** ser **exclusivamente** um JSON **válido**, sem texto adicional.
-        - **Formato exato da resposta:**
-
-        {{
-            "resposta1": true/false,
-            "resposta2": número ou null,
-            "resposta3": "categoria" ou null
-        }}
-
-        Responda **apenas** com este JSON, sem explicações ou texto adicional.
-        """
-
-        # Fazer uma pergunta ao modelo
-        response = self.model.generate_content(prompt)
-        print(response.text)
-        # Converter a resposta para JSON
-        try:
-            respostas = json.loads(re.sub(r'```json|```', '', response.text).strip())
-            message.is_expense = respostas["resposta1"]
-            message.valor = respostas["resposta2"]
-            message.categoria = respostas["resposta3"]
-        except json.JSONDecodeError:
-            print("Erro ao processar a resposta:", response.text)
-
-        return message
-
-class DBManager:
+class DBmanager:
     def __init__(self):
         pass
-    def insert(self, message):
+
+    def save(self, message):
+        # caso qualquer erro, raise exption
         pass
+
+    def get(self, user_id):
+        pass
+
+    def update(self, message):
+        pass
+
+    def delete(self, user_id):
+        pass
+
+    def select_user(self, user_id):
+        users = [1234, 3333,2900, 6799506447]
+        if user_id in users:
+            return True
+        return False
+
+
+def handler_mensage(menssage_text, user_id):
+    msg = Message(menssage_text, user_id)
+    if not msg.check_user():
+        raise ValueError("User not found")
+
+    llm = GeminiPipeline()
+    info = llm.run_all(msg.texto)
+    # PickleFy().serialize(file_name='info', variavel=info)
+
+    # info = PickleFy().serialize(file_name='info')
+    try:
+        msg.definir_despesa(info['is_expense'], info['amount'], info['category'])
+    except ValueError as e:
+        raise e
+    try:
+        db = DBmanager()
+        db.save(msg)
+    except Exception as e:
+        e.message = f"Error to save the message: {e.message}"
+        raise e
+
+    return msg
+
 
 if __name__ == "__main__":
-    #main()
-    mensage_obj = Message("5 reais no transporte de seu zeca", 1234)
-    gen_app = GeminiIA()
-    mensage_obj = gen_app.get_expense(mensage_obj)
-    DBManager().insert(message)
-    # If mensagem inserida e tudo ok, return mensagem inserida
-    # else menssagem deu erro e o porque?
-    print("End")
 
-    # Precisa verificar se o usuario da mensagem esta no BD
-    # Destinguir mensagens de gasto das comuns
-    # categorizar o gasto
-    """
-    ● Python 3.8+ required.
-    ● Must handle concurrent requests.
-    ● Use LangChain with a supported LLM.
-    ● PostgreSQL database.
-    """
+
+    tg_bot = TelegramBot()
+    tg_bot.main()
+
